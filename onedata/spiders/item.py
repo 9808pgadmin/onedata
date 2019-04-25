defae863f3d88747670864c37acc8147db2927f6
9808pgadmin <980865707@qq.com>
2019-04-25 10:27:33 +0800

    success

--- onedata/spiders/item.py
+++ onedata/spiders/item.py
@@ -5,15 +5,15 @@ class itemer(scrapy.Spider):
     start_urls = ['http://lab.scrapyd.cn']
 
     def parse(self,response):
-        paqu1 = response.css('div.quote')[0]
+        paqu1 = response.css('div.quote')[1]
 
         text = paqu1.css('.text::text').extract_first()
-        autor = paqu1.css('.autor::text').extract_first()
-        tags = paqu1.css('.tags .tag::text').extract_first()
+        autor = paqu1.css('.author::text').extract_first()
+        tags = paqu1.css('.tags .tag::text').extract()
         tags = ",".join(tags)
 
-        filename = '%s-语录.txt' % autor
-        f = open(filename,"a+")
+        filename = '%s-语录.txt' %autor
+        f = open(filename,"a+",encoding='utf-8')
         f.write(text)
         f.write('\n')
         f.write('标签: '+tags)
